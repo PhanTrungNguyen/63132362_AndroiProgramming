0b379695f8e4ffc9b2925723c1a1ada0982f6408
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-03-23 18:56:07 +0700

    Tạo project
    Bắt đầu dự án.

---
+++ Cau2_Ransanmoi/app/src/main/java/ntu63132362/cau1_Ransanmoi/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu63132362.cau1_Ransanmoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
