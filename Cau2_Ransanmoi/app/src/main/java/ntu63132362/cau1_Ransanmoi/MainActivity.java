d62d1f6881823a1ad9121e04d3d288b13f31f4c4
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-03-23 19:52:20 +0700

    Lần 4
    Bắt đầu thêm chức này vào các button đã tạo

--- Cau2_Ransanmoi/app/src/main/java/ntu63132362/cau1_Ransanmoi/MainActivity.java
+++ Cau2_Ransanmoi/app/src/main/java/ntu63132362/cau1_Ransanmoi/MainActivity.java
@@ -1,14 +1,73 @@
 package ntu63132362.cau1_Ransanmoi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
+    private SurfaceView surfaceView;
+    private TextView scoreTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        scoreTV = findViewById(R.id.scoreTV);
+
+        final AppCompatImageButton topButton = findViewById(R.id.topButton);
+        final AppCompatImageButton leftButton = findViewById(R.id.leftButton);
+        final AppCompatImageButton rightButton = findViewById(R.id.rightButton);
+        final AppCompatImageButton bottomButton = findViewById(R.id.bottomButton);
+
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
     }
 }
