f4a84e37ef7d296ae1d9f76dc688b92fbb626439
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-03-24 19:58:50 +0700

    Lần 18
    Sửa lại đường dẫn

---
+++ Cau3_GTBanThan/app/src/main/java/ntu63132362/Cau3_GTBanThan/MainActivity.java
@@ -0,0 +1,19 @@
+package ntu63132362.Cau3_GTBanThan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void backToMain (View v){
+        finish();
+    }
+}
