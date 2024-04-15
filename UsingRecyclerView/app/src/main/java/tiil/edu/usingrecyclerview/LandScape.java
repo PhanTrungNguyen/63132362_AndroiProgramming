30bb6d002c035be89844fdb46811c0b99d8cf385
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-04-15 20:52:53 +0700

    Hoàn thành bài tập onl

---
+++ UsingRecyclerView/app/src/main/java/tiil/edu/usingrecyclerview/LandScape.java
@@ -0,0 +1,26 @@
+package tiil.edu.usingrecyclerview;
+
+public class LandScape {
+    String imgLandFileName, landCaption;
+
+    public LandScape(String imgLandFileName, String landCaption) {
+        this.imgLandFileName = imgLandFileName;
+        this.landCaption = landCaption;
+    }
+
+    public String getImgLandFileName() {
+        return imgLandFileName;
+    }
+
+    public void setImgLandFileName(String imgLandFileName) {
+        this.imgLandFileName = imgLandFileName;
+    }
+
+    public String getLandCaption() {
+        return landCaption;
+    }
+
+    public void setLandCaption(String landCaption) {
+        this.landCaption = landCaption;
+    }
+}
