162fa4e7430d0bd109eae3a5a6ce3b18abec48bd
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-04-26 10:15:15 +0700

    ExplicitIntent
    ví dụ ve intent

---
+++ SimpleExplicitIntents/app/src/main/java/phantrungnguyen/pt63132362/MainActivity2.java
@@ -0,0 +1,20 @@
+package phantrungnguyen.pt63132362;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void chuyenManHinh(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
