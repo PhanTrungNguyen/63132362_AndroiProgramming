17b685264a51c323eaf389c1f12ecde0e118ebbb
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-05-10 13:08:59 +0700

    UsingSQLLite
    UsingSQLLite

---
+++ DuAnCuoiKi/app/src/main/java/duancuoiki/phantrungnguyen63132362/Activitis/MainActivity.java
@@ -0,0 +1,16 @@
+package duancuoiki.phantrungnguyen63132362.Activitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import duancuoiki.phantrungnguyen63132362.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- UsingSQLLite/app/src/main/java/tiil/edu/usingsqllitenguyen/MainActivity.java
+++ UsingSQLLite/app/src/main/java/tiil/edu/usingsqllitenguyen/MainActivity.java
@@ -1,4 +1,4 @@
-package duancuoiki.phantrungnguyen63132362;
+package tiil.edu.usingsqllitenguyen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.book_item);
     }
 }
