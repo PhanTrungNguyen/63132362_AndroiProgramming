32958dca6ca56982e6b722a09b0a1797bff180e4
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-05-14 18:21:09 +0700

    Khởi tạo dự án QuizApp
    Tạo project

---
+++ QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/MainActivity.java
@@ -0,0 +1,14 @@
+package phantrungnguyen63132362.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
