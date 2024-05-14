be2e519005a4fd5dbcc4def901cd8925fcbbf4da
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-05-14 19:22:37 +0700

    Lần 5
    Bắt đầu tạo tính năng cho từng nút bấm trên màn hình

--- QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/playActivity.java
+++ QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/playActivity.java
@@ -3,6 +3,8 @@ package phantrungnguyen63132362.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class    playActivity extends AppCompatActivity {
     String[] question_list = {"Kết quả của phép tính 23+17 là bao nhiêu?"
@@ -18,9 +20,20 @@ public class    playActivity extends AppCompatActivity {
             "54","55","56","57"
     };
     String[] correct_list = {"40","34","105","45","55"};
+    TextView cpt_question , text_question;
+    Button btn_choose1 , btn_choose2 , btn_choose3 , btn_choose4 , btn_next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        cpt_question = findViewById(R.id.cpt_question);
+        text_question = findViewById(R.id.text_question);
+        
+        btn_choose1 = findViewById(R.id.btn_choose1);
+        btn_choose2 = findViewById(R.id.btn_choose2);
+        btn_choose3 = findViewById(R.id.btn_choose3);
+        btn_choose4 = findViewById(R.id.btn_choose4);
+        btn_next = findViewById(R.id.btn_next);
     }
 }
