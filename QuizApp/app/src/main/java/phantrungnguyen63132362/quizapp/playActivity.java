660c5bfa42ad0175c836da29255fc6a0ee6ac017
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-05-14 19:16:59 +0700

    Lần 4
    Tạo list câu hỏi, câu trả lời, câu trả lời đúng

--- QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/playActivity.java
+++ QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/playActivity.java
@@ -5,7 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class    playActivity extends AppCompatActivity {
-    String[] question_list = {"Cau1","Cau2","Cau3","Cau4"};
+    String[] question_list = {"Kết quả của phép tính 23+17 là bao nhiêu?"
+            ,"Kết quả của phép tính 58-24 là bao nhiêu?"
+            ,"Kết quả của phép tính 76+29 là bao nhiêu?"
+            ,"Kết quả của phép tính 90-45 là bao nhiêu?"
+            ,"Kết quả của phép tính 33+22 là bao nhiêu?"
+    };
+    String[] choose_list = {"39","40","41","42",
+            "32","34","36","38",
+            "103","104","105","106",
+            "40","43","45","50",
+            "54","55","56","57"
+    };
+    String[] correct_list = {"40","34","105","45","55"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
