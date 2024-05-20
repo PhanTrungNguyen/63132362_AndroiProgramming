171d6ba7ee34ed61d2d72aa2cc0a09fda296f545
PhanTrungNguyen <145771479+PhanTrungNguyen@users.noreply.github.com>
2024-05-20 21:52:45 +0700

    Lần 13
    Làm giao diện và chức năng back cho setting

--- QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/settingActivity.java
+++ QuizApp/app/src/main/java/phantrungnguyen63132362/quizapp/settingActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class settingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        findViewById(R.id.image_back).setOnClickListener(
+                a-> finish()
+        );
     }
+
 }
